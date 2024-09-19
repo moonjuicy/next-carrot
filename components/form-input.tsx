40ad37bf93ae277ed42cd97da1118c8ec0346c7a
Moonjuice <moon.kang@inka.co.kr>
2024-09-19 12:34:31 +0900

    implement Zod for data validation in forms

--- components/form-input.tsx
+++ components/form-input.tsx
@@ -2,7 +2,7 @@ interface FormInputProps {
   type: string;
   placeholder: string;
   required: boolean;
-  errors: string[];
+  errors?: string[];
   name: string;
 }
 
@@ -22,7 +22,7 @@ export default function FormInput({
         placeholder={placeholder}
         required={required}
       />
-      {errors.map((error, index) => (
+      {errors && errors.map((error, index) => (
         <span key={index} className="text-red-500 font-medium">
           {error}
         </span>
