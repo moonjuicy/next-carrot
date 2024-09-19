99089571c97d955e27f97600641a6b4c48282edb
Moonjuice <moon.kang@inka.co.kr>
2024-09-19 12:08:23 +0900

    set up api for login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,25 +1,37 @@
-import FormButton from "@/components/form-btn";
-import FormInput from "@/components/form-input";
-import SocialLogin from "@/components/social-login";
+import FormButton from '@/components/form-btn';
+import FormInput from '@/components/form-input';
+import SocialLogin from '@/components/social-login';
 
 export default function LogIn() {
-  return (
-    <div className="flex flex-col gap-10 py-8 px-6">
-      <div className="flex flex-col gap-2 *:font-medium">
-        <h1 className="text-2xl">안녕하세요!</h1>
-        <h2 className="text-xl">Log in with email and password.</h2>
-      </div>
-      <form className="flex flex-col gap-3">
-        <FormInput type="email" placeholder="Email" required errors={[]} />
-        <FormInput
-          type="password"
-          placeholder="Password"
-          required
-          errors={[]}
-        />
-        <FormButton loading={false} text="Log in" />
-      </form>
-      <SocialLogin />
-    </div>
-  );
+    async function handleForm(formData: FormData) {
+        'use server';
+        console.log(formData.get('email'), formData.get('password'));
+        console.log('i run in the server baby!');
+    }
+    return (
+        <div className="flex flex-col gap-10 py-8 px-6">
+            <div className="flex flex-col gap-2 *:font-medium">
+                <h1 className="text-2xl">안녕하세요!</h1>
+                <h2 className="text-xl">Log in with email and password.</h2>
+            </div>
+            <form className="flex flex-col gap-3" action={handleForm}>
+                <FormInput
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                    required
+                    errors={[]}
+                />
+                <FormInput
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    required
+                    errors={[]}
+                />
+                <FormButton loading={false} text="Log in" />
+            </form>
+            <SocialLogin />
+        </div>
+    );
 }
